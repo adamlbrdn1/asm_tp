275d5e62062e6a45feefc33842e74f6f352abbed
adamlbrdn1 <131999689+adamlbrdn1@users.noreply.github.com>
2024-04-14 23:15:45 +0200

    Update asm02.s

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,42 +1,57 @@
-global _start
-section .text
-
-_start:
-	mov rax, 0
-	mov rdi, 0
-	mov rsi, input
-	mov rdx, 1
-	syscall
-
-
-	movzx rdi, byte [input]
-	sub rdi, '0'
-
-	
-	cmp rdi, 42
-	jne not_42
-	
-
-	
-	mov rax, 0x1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx message_len
-	syscall
-
-	
-	mov rax, 0x3c
-	mov rdi, 1
-	syscall
-
-
-not_42:
-	mov rax, 0x3c
-	mov rdi, 1
-	syscall
-
+section .bss
+    input resb 3  
 
 section .data
-	message db '1337', 0xA
-	message_len equ $ - message 
-	input resb 1
+    msg db "1337", 10  
+    msg_len equ $ - msg  
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0        
+    mov rdi, 0        
+    mov rsi, input    
+    mov rdx, 3        
+    syscall
+
+    
+    cmp byte [input], '4'
+    jne _error
+    
+    
+    cmp byte [input + 1], '2'
+    jne _error
+
+    
+    cmp byte [input + 2], 10
+    jne _error
+
+    
+    mov rax, 1        
+    mov rdi, 1        
+    mov rsi, msg      
+    mov rdx, msg_len  
+    syscall
+
+   
+    mov rax, 60       
+    xor rdi, rdi      
+    syscall
+
+_error:
+    mov rax, 60       
+    mov rdi, 1        
+    mov rsi, errmsg   
+    mov rdx, errmsg_len  
+    syscall
+
+    
+    mov rax, 60       
+    mov rdi, 1        
+    syscall
+
+section .data
+    errmsg db "Erreur: Entrée incorrecte", 10  
+    errmsg_len equ $ - errmsg 
+
