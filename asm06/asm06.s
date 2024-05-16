9bd95b4a869f8af363c88ddef64f7b283360a6f3
adamlbrdn1 <131999689+adamlbrdn1@users.noreply.github.com>
2024-05-16 12:24:28 +0200

    Update asm06.s

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,75 +1,76 @@
 global _start
 
 section .bss
-    input resb 10  ; Réserver de l'espace pour stocker l'entrée
+    input resb 32  ; Réservation de 32 octets pour l'entrée utilisateur
 
 section .text
 _start:
 
-    ; Lire l'entrée
-    mov rax, 0          ; sys_read
-    mov rdi, 0          ; stdin
-    mov rsi, input      ; Adresse de stockage
-    mov rdx, 10         ; Nombre maximum de caractères à lire
+    ; Lecture de l'entrée utilisateur
+    mov rax, 0      ; Code syscall pour sys_read
+    mov rdi, 0      ; Descripteur de fichier stdin
+    mov rsi, input ; Pointeur vers l'emplacement où stocker l'entrée
+    mov rdx, 32     ; Nombre maximum d'octets à lire
     syscall
 
-    ; Convertir l'entrée en un nombre entier
-    xor rax, rax        ; Réinitialiser RAX
-    mov rcx, input      ; Pointeur vers le début de la chaîne
-.convert_loop:
-    movzx rdx, byte [rcx]  ; Charger le caractère
-    test rdx, rdx          ; Vérifier la fin de la chaîne
-    jz .input_done         ; Si c'est la fin de la chaîne, terminer la conversion
-    sub rdx, '0'           ; Convertir ASCII en nombre
-    imul rax, rax, 10      ; Multiplier par 10
-    add rax, rdx           ; Ajouter le chiffre
-    inc rcx                ; Passer au caractère suivant
-    jmp .convert_loop      ; Répéter la conversion
+    ; Conversion de la chaîne d'entrée en nombre entier
+    mov rdi, 0      ; Indice pour parcourir la chaîne d'entrée
+    xor r8, r8      ; Initialise le résultat à zéro
+    xor rax, rax    ; Réinitialisation d'AL à zéro
+convert_loop:
+    mov al, [input + rdi]  ; Charge un caractère de la chaîne d'entrée dans AL
+    cmp al, 10             ; Comparaison avec la valeur ASCII de la nouvelle ligne
+    je input_done          ; Si c'est une nouvelle ligne, terminer la saisie
+    cmp al, '0'            ; Comparaison avec le caractère '0'
+    jl error               ; Si c'est inférieur à '0', c'est une erreur
+    cmp al, '9'            ; Comparaison avec le caractère '9'
+    jg error               ; Si c'est supérieur à '9', c'est une erreur
+    sub al, 48             ; Convertit le caractère ASCII en nombre entier
+    imul r8, r8, 10        ; Multiplie le résultat actuel par 10
+    add r8, rax            ; Ajoute la valeur convertie
+    inc rdi                ; Passage au prochain caractère
+    jmp convert_loop       ; Boucle pour le prochain caractère
 
-.input_done:
+input_done:
+    ; Vérification de la primalité du nombre
+    xor rdi, rdi       ; Réinitialisation de RDI à zéro pour utilisation future
+    xor rax, rax       ; Réinitialisation de RAX à zéro
+    mov rcx, 2         ; Débute la vérification à partir de 2
 
-    ; Traiter les cas particuliers
-    cmp rax, 1
-    je _not_prime       ; 1 n'est pas un nombre premier
-    cmp rax, 2
-    je _prime           ; 2 est un nombre premier
+    cmp r8, 1          ; Si le nombre est égal à 1
+    je not_prime       ; Il n'est pas premier
 
-    ; Vérifier si le nombre est pair
-    test al, 1               
-    jnz _check_divisors                  
-    
-    ; Si le nombre est pair et différent de 2, il n'est pas premier
-    mov rax, 0
-    jmp _exit
+    cmp r8, 2          ; Si le nombre est égal à 2
+    je prime           ; Il est premier
 
-_check_divisors:
-    ; Vérifier les diviseurs jusqu'à la racine carrée du nombre
-    mov rdx, 2                ; Commencer par le premier diviseur (2)
-.check_divisors_loop:
-    mov rsi, rax              ; Sauvegarder le nombre
-    xor rax, rax              ; Réinitialiser RAX
-    mov rcx, rsi              ; Charger le nombre à diviser
-    div rdx                   ; Diviser rsi:rax par rdx (rax = quotient, rdx = reste)
-    cmp rdx, 0                ; Vérifier si le reste est nul
-    je _not_prime             ; Si oui, le nombre n'est pas premier
-    inc rdx                   ; Passer au diviseur suivant
-    cmp rdx, rsi              ; Vérifier si nous avons dépassé la racine carrée du nombre
-    jg _prime                 ; Si oui, le nombre est premier
-    jmp .check_divisors_loop  ; Sinon, continuer la vérification des diviseurs
+check_loop:
+    xor rdx, rdx       ; Réinitialisation de RDX à zéro
+    mov rax, r8        ; Charge le nombre à vérifier dans RAX
+    div rcx            ; Divise RAX par RCX, le quotient est stocké dans RAX et le reste dans RDX
+    cmp rdx, 0         ; Vérifie si le reste est zéro
+    je check           ; Si oui, le nombre n'est pas premier
+    inc rcx            ; Passe au prochain nombre pour la vérification
+    jmp check_loop    ; Boucle pour le prochain nombre
 
-_prime:
-    ; Si le nombre est premier, retourner 1
-    mov rax, 1
-    jmp _exit
+check:
+    cmp rcx, r8        ; Comparaison de RCX avec le nombre d'origine
+    je prime           ; Si RCX est égal au nombre d'origine, le nombre est premier
+    jne not_prime      ; Sinon, le nombre n'est pas premier
 
-_not_prime:
-    ; Si le nombre n'est pas premier, retourner 0
-    mov rax, 0
-    jmp _exit
-
-_exit:
-    ; Terminer le programme
-    mov rdi, rax       
-    mov rax, 60
+prime:
+    ; Si le nombre est premier, retourner 0
+    mov rax, 60      ; Code syscall pour sys_exit
+    mov rdi, 0       ; Code de retour 0
     syscall
 
+not_prime:
+    ; Si le nombre n'est pas premier, retourner 1
+    mov rax, 60      ; Code syscall pour sys_exit
+    mov rdi, 1       ; Code de retour 1
+    syscall
+
+error:
+    ; Gestion des erreurs
+    mov rax, 60      ; Code syscall pour sys_exit
+    mov rdi, 1       ; Code de retour 1 pour les erreurs
+    syscall
